f52d0ce57ecbafd2b50f8b446e13259816750bed
anushka.jain <anushka.jain@innovasolutions.com>
2026-01-01 17:18:18 +0530

    text onclick implemented

--- app/src/main/java/com/example/splashscreenjc/MainActivity.kt
+++ app/src/main/java/com/example/splashscreenjc/MainActivity.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.splashscreenjc.ui.theme.SplashScreenJcTheme
@@ -39,11 +40,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LearnTextAndModifiers(){
+fun LearnTextAndModifiers() {
+    val clickOnText = { }
+
     Text(
-        text = stringResource(R.string.hello_text),
+        text = "hello",
         color = Color.Red,
         fontSize = 32.sp,
-        fontStyle = FontStyle.Italic
+        fontStyle = FontStyle.Italic,
+        modifier = Modifier.padding(22.dp)
+            .background(Color.Black)
+            .clickable(onClick = clickOnText)
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowPreview() {
+    SplashScreenJcTheme {
+        LearnTextAndModifiers()
+    }
 }
